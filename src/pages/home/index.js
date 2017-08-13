1fa0da0d1a22a3535ee865e31a3dff5628f8375b
Rossouw Binedell <ross@cowboyaliens.com>
2017-08-13 21:03:45 +0200

    Added SCSS tothe project

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Subview1 from "./subview1";
 import Subview2 from "./subview2";
+import Subview3 from "./subview3";
 import "./style.css";
 
 // Main container for the homescreen
@@ -10,6 +11,7 @@ class Home extends Component {
       <div >
         <Subview1 />
         <Subview2 />
+        <Subview3 />
       </div>
     );
   }
