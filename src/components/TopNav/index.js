d0d75eebc17745658dbca93fc3e7da1f406e5e13
Keanu <keanu@cowboyaliens.com>
2017-08-13 23:59:14 +0200

    Basic Top Nav
    Commented Out SubView3, Causes Error for some reason.

---
+++ src/components/TopNav/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import "./style.css";
+import logo from "./../../assets/images/ado-logo.png";
+
+export default class TopNav extends Component {
+  render() {
+    return (
+       <div className="container">
+        <a className="logo" href="/"><img src={logo} alt="..."/></a>
+        <a className="last-nav-a top-nav-a" href="">Contact</a>
+        <div className="dropdown">
+            <button className="dropbtn">Careers</button>
+            <div className="dropdown-content">
+            <a className="top-nav-a" href="/graduate/form">Graduate Programme</a>
+            <a className="top-nav-a" href="/">Vacation Worker</a>
+            </div>
+        </div> 
+        <a className="top-nav-a" href="">Work</a>
+        </div> 
+    );
+  }
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Subview1 from "./subview1";
 import Subview2 from "./subview2";
-import Subview3 from "./subview3";
+// import Subview3 from "./subview3";
 import "./style.css";
+import TopNav from "./../../components/TopNav/index.js";
 
 // Main container for the homescreen
 class Home extends Component {
   render() {
     return (
       <div >
+        <TopNav/>
         <Subview1 />
         <Subview2 />
-        <Subview3 />
+        {/*<Subview3 />*/}
       </div>
     );
   }
