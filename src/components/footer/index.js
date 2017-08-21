c49799387f0d2ebd34b9d55d541975d4474d3c2e
Rossouw Binedell <ross@cowboyaliens.com>
2017-08-21 23:10:37 +0200

    ADDED: Footer and some other changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,9 +3,6 @@ import "./style.css";
 
 import data from "../../assets/data/EN/footer-info.json";
 
-import facebook from "../../assets/images/facebook.svg";
-import youtube from "../../assets/images/youtube.svg";
-import linkedin from "../../assets/images/linkedin.svg";
 import absa from '../../assets/images/absa.svg';
 
 export default class Footer extends Component {
@@ -28,8 +25,7 @@ export default class Footer extends Component {
   }
 
   render() {
-    if (this.state.width >= 1100) {
-
+    if (this.state.width > 1100) {
 
       return (
         <div className="footer">
@@ -54,87 +50,37 @@ export default class Footer extends Component {
 
           <div className="footer-bottom">
             <span>PRIVACY & COOKIES POLICY 2017 ADO</span>
-            <div className="footer-share-images">
-              <a href="http://facebook.com">
-                <img src={facebook} alt="facebook page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={youtube} alt="youtube page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={linkedin} alt="linkedin share"/>
-              </a>
-            </div>
             <img className="footer-absa-logo" src={absa} alt="absa logo" />
           </div>
 
         </div>
       );
-    } else if (this.state.width >= 680) {
+    } else if (this.state.width > 680) {
 
 
       return (
         <div className="footer">
           <div className="footer-top">
-            <div className="footer-column-1">
-              <div className="footer-item-1">
-                <span>{data.title}</span>
-              </div>
-              <div className="footer-item-3">
-                <span>{data.p1}</span>
-                <p>{data.p2}</p>
-                <a>{data.a1}</a>
-              </div>
+            <div className="footer-item-1">
+              <span>{data.title}</span>
             </div>
-            <div className="footer-column-2">
-              <div className="footer-item-2">
-                <span>{data.contact1.title}</span>
-                <p>{data.contact1.content}</p>
-              </div>
-              <div className="footer-item-2">
-                <span>{data.contact2.title}</span>
-                <p>{data.contact2.content}</p>
-              </div>
+            <div className="footer-item-2">
+              <span>{data.contact1.title}</span>
+              <p>{data.contact1.content}</p>
+            </div>
+            <div className="footer-item-2">
+              <span>{data.contact2.title}</span>
+              <p>{data.contact2.content}</p>
             </div>
           </div>
+          <div className="footer-item-3">
+            <a>{data.a1}</a>
+          </div>
           
-          {
-            this.state.width >= 850? 
-            
-            (<div className="footer-bottom">
+          <div className="footer-bottom">
             <span>PRIVACY & COOKIES POLICY 2017 ADO</span>
-            <div className="footer-share-images">
-              <a href="http://facebook.com">
-                <img src={facebook} alt="facebook page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={youtube} alt="youtube page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={linkedin} alt="linkedin share"/>
-              </a>
-            </div>
             <img className="footer-absa-logo" src={absa} alt="absa logo" />
-          </div>):
-
-          (<div>
-            <div className="footer-share-images">
-              <a href="http://facebook.com">
-                <img src={facebook} alt="facebook page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={youtube} alt="youtube page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={linkedin} alt="linkedin share"/>
-              </a>
-            </div>
-            <div className="footer-bottom">
-              <span>PRIVACY & COOKIES POLICY 2017 ADO</span>
-              <img className="footer-absa-logo" src={absa} alt="absa logo" />
-            </div>
-          </div>)
-          }
+          </div>
 
         </div>
       );
@@ -163,17 +109,6 @@ export default class Footer extends Component {
           </div>
 
           <div className="footer-bottom">
-            <div className="footer-share-images">
-              <a href="http://facebook.com">
-                <img src={facebook} alt="facebook page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={youtube} alt="youtube page"/>
-              </a>
-              <a href="http://facebook.com">
-                <img src={linkedin} alt="linkedin share"/>
-              </a>
-            </div>
             <span>PRIVACY & COOKIES POLICY 2017 ADO</span>
             <img className="footer-absa-logo" src={absa} alt="absa logo" />
           </div>
