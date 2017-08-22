84a603635c9397377a0cd7a009e76bc5acd0615f
Rossouw Binedell <ross@cowboyaliens.com>
2017-08-22 17:37:37 +0200

    Added some stuff

--- src/pages/graduateInfo/index.js
+++ src/pages/graduateInfo/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import "./style.css";
 
-// Rooting to be added later
+import TopNav from '../../components/TopNav';
+import Footer from '../../components/footer';
+import Expectation from './expectations';
+
+import data from "../../assets/data/EN/graduate-info.json";
+import expectation1 from '../../assets/images/ADO-Image.png';
+import expectation2 from '../../assets/images/ADO-Image.png';
+import expectation3 from '../../assets/images/ADO-Image.png';
+import expectation4 from '../../assets/images/ADO-Image.png';
+import expectation5 from '../../assets/images/ADO-Image.png';
+import mainImage from '../../assets/images/ADO-Image.png';
+
+const images = [expectation1, expectation2, expectation3, expectation4, expectation5];
+
 export default class GraduateInfo extends Component {
   render() {
     return (
-      <h1>Info Page 1</h1>
+      <div>
+        <TopNav />
+        <div className="content">
+          <h1 className="main-title">{data.t1}</h1>
+          <h2 className="secondary-title">{data.t2}</h2>
+          <div className="expectation-container">
+            {
+              data.expectations.map((item, i) => {
+                return (<Expectation image={images[i]} text={item} />)
+              })
+            }
+          </div>
+          <div className="main-text-container">
+            <p>{data.p1}</p>
+            <img src={mainImage} alt="Group Photo"/>
+          </div>
+        </div>
+        <Footer />
+      </div>
     );
   }
 }
